255cd78d1ad05e1d9a411e2ff3775f52ae7f89f2
Bach <borislukovic@outlook.com>
2019-07-25 10:58:32 +0200

    config passport, add reg route and template

--- app.js
+++ app.js
@@ -3,9 +3,13 @@ var app = express();
 var port = process.env.PORT || 3000;
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
+var passport = require("passport");
+var LocalStrategy = require("passport-local");
+
 // Schema setup
 var Campground = require("./models/campgrounds");
 var Comment = require("./models/comment");
+var User = require("./models/user");
 var seedDB = require("./seeds");
 
 mongoose.connect("mongodb://localhost/yelp_camp", {useNewUrlParser: true});
@@ -13,6 +17,18 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 seedDB();
+
+// PASSPORT CONFIGURATION
+app.use(require("express-session")({
+    secret: "Project passport secret",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", function(req, res){
     res.render("landing");
@@ -103,6 +119,26 @@ app.post("/campgrounds/:id/comments", function(req, res){
                 }
             });
         }
+    });
+});
+
+//AUTH ROUTES
+
+//Show register form
+app.get("/register", function(req, res){
+    res.render("register");
+});
+//Handle sign up logic
+app.post("/register", function(req, res){
+    var newUser = new User({username: req.body.username});
+    User.register(newUser, req.body.password, function(err, user){
+        if(err){
+            console.log(err);
+            return res.render("register");
+        }
+        passport.authenticate("local")(req, res, function(){
+            res.redirect("/campgrounds");
+        });
     });
 });
 
